d6417c1e422156d737221e1c6219f4f4db434a33
caldeiratalles <caldeiratalles@hotmail.com>
2021-11-01 17:43:20 -0300

    os outros commits quebraram

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
         <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
           <p className="text-light">App desenvolvido por <a href="https://github.com/caldeiratalles" target="_blank" rel="noreferrer">Talles Marcelo</a></p>
-          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+          <p className="text-light"><small><strong>Semana Spring React :D</strong><br/>
             Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
         </div>
       </footer>
